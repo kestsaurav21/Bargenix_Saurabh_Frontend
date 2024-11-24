b3c64d89ebc4869d04fd452c9009752ebe5f14ac
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-25 02:37:38 +0530

    Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Chatbox from '../components/ChatBox'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <Chatbox />
+    </div>
+  )
+}
+
+export default Home
