6e0006fae2bd31cae5a02462228ee3af3906a8c7
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-24 15:57:00 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaLinkedin, FaXTwitter } from "react-icons/fa6";
+
 const Footer = () => {
     return(
-        <div className="bg-black py-6 px-4 sm:px-10 md:px-14 lg:px-28 flex justify-center items-center text-white shadow-lg">
-            Copyright © 2024 - ChatBot
-        </div>
+    <footer className="w-full px-4 py-8 bg-black flex flex-col items-center justify-evenly">
+      <div className="flex flex-col items-center gap-4 md:flex-row md:gap-8">
+        <p className="text-white text-sm font-bold text-center md:text-left md:text-base lg:text-xl">
+          All rights reserved. © 2024 | ChatBot
+        </p>
+      </div>
+      <div className="flex gap-4 mt-4 md:mt-0">
+        <FaXTwitter className="text-white" size="1.5rem" />
+        <FaLinkedin className="text-white" size="1.5rem" />
+        <FaInstagramSquare className="text-white" size="1.5rem" />
+      </div>
+    </footer>
+        
     )
 
 
