d8b643b624a995a9fcbedcb7aa39f5633ee0313b
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-24 15:46:54 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return(
+        <div className="bg-black py-6 px-4 sm:px-10 md:px-14 lg:px-28 flex justify-center items-center text-white shadow-lg">
+            Copyright © 2024 - ChatBot
+        </div>
+    )
+
+
+};
+
+
+export default Footer;
