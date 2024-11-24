2903c816d21160dfa7dbd6f8d1137d38bd739e75
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-25 02:37:28 +0530

    Routes Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import About from './pages/About'
+import Pricing from './pages/Pricing'
+import Features from './pages/Features'
+import Contact from './pages/Contact'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
      <NavBar />
-
+      <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/features' element={<Features />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
      <Footer />
+     </BrowserRouter>
     </>
   )
 }
