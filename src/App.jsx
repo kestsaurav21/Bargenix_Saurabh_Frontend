dc7cfd4205e44b8a4fe994d38d2a0a33edcd1df8
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-24 15:34:49 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
